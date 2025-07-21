e0234e604cd011e13b90b58687fa89670f713c07
Shao <romancaseres00@gmail.com>
2025-07-21 23:33:42 +0800

    Prepare app for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/outfithaven">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/outfithaven/", // 👈 Add this line — MUST match your repo name!
+  base: "/", // 👈 Add this line — MUST match your repo name!
   server: {
     port: 3000,
     host: true,
